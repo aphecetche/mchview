0fb53ad5bd48d58db533a385aa7d45aa07fe27c6
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2019-02-11 22:55:50 +0100

    Just playing with linting

--- src/views/MchViewPort.js
+++ src/views/MchViewPort.js
@@ -1,37 +1,32 @@
-import './mchviewport.css';
-import m from 'mithril';
-import drawOutline from './draw.js';
-import ShowOutline from '../models/ShowOutline';
-import Segmentation from '../models/Segmentation';
-import ShowElement from '../models/ShowElement';
+import "./mchviewport.css";
+import m from "mithril";
+import drawOutline from "./draw.js";
+import ShowOutline from "../models/ShowOutline";
+import Segmentation from "../models/Segmentation";
+import ShowElement from "../models/ShowElement";
 
 const MchViewPort = {
-  current: {
-  },
+  current: {},
   view: () => {
-    return m('div', {class: 'mchviewport'},
-      m('header'));
+    return m("div", { class: "mchviewport" }, m("header"));
   },
   oninit: function() {
-    this.current.deid=ShowElement.deid;
-    this.current.bending=ShowElement.bending;
-    console.log('MchViewPort oninit');
+    this.current.deid = ShowElement.deid;
+    this.current.bending = ShowElement.bending;
   },
   oncreate: () => {
     Segmentation.loadData(ShowElement.deid, ShowElement.bending);
-    console.log('MchViewPort oncreate');
     drawOutline(ShowOutline, Segmentation);
   },
   onupdate: function() {
-    if (this.current.deid != ShowElement.deid ||
-          this.current.bending != ShowElement.bending) {
+    if (
+      this.current.deid != ShowElement.deid ||
+      this.current.bending != ShowElement.bending
+    ) {
       Segmentation.loadData(ShowElement.deid, ShowElement.bending);
     }
-    console.log('MchViewPort onupdate' + this.current.deid + ' / ' + this.current.bending);
-    console.log('=>' + ShowElement.deid + ' / ' + ShowElement.bending);
-
-    this.current.deid=ShowElement.deid;
-    this.current.bending=ShowElement.bending;
+    this.current.deid = ShowElement.deid;
+    this.current.bending = ShowElement.bending;
     drawOutline(ShowOutline, Segmentation);
   }
 };
